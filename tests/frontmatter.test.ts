6e56326e15d4017ade63d1876e5d2db37ac1e4e2
VChet <17050347+VChet@users.noreply.github.com>
2025-06-18 23:42:25 +0500

    test: add frontmatter validator

---
+++ tests/frontmatter.test.ts
@@ -0,0 +1,32 @@
+import { readdirSync, readFileSync } from "node:fs";
+import { basename, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import matter from "gray-matter";
+import { describe, expect, it } from "vitest";
+
+describe("frontmatter validator", () => {
+  function getMarkdownFiles(dir: string): string[] {
+    return readdirSync(dir)
+      .filter((fileName) => fileName.endsWith(".md"))
+      .map((fileName) => join(dir, fileName));
+  }
+  const ARTICLES_DIR = fileURLToPath(new URL("../articles", import.meta.url));
+
+  for (const filePath of getMarkdownFiles(ARTICLES_DIR)) {
+    const raw = readFileSync(filePath, "utf8");
+    const { data: frontmatter } = matter(raw);
+
+    it(basename(filePath), () => {
+      expect(frontmatter.title).toBeTypeOf("string");
+      expect(frontmatter.description).toBeTypeOf("string");
+      expect(frontmatter.tags).toSatisfy(Array.isArray).and.not.toHaveLength(0);
+
+      if ("publish" in frontmatter) {
+        expect(frontmatter.publish).toBe(false);
+        expect(frontmatter.date).toBeUndefined();
+      } else {
+        expect(frontmatter.date).toBeInstanceOf(Date);
+      }
+    });
+  }
+});
