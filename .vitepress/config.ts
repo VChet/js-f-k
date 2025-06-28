95b68354f8b90f3c95944e7eebdd0065f90b8870
VChet <17050347+VChet@users.noreply.github.com>
2025-06-29 03:49:26 +0500

    feat(theme): use default theme, use GitHub theme for syntax highlighting

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -66,9 +66,7 @@ export default defineConfig({
       }
     }
   },
-  markdown: {
-    theme: { light: "catppuccin-latte", dark: "catppuccin-mocha" }
-  },
+  markdown: { theme: { light: "github-light", dark: "github-dark" } },
   lastUpdated: true,
   themeConfig: {
     outline: false,
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
-import "@catppuccin/vitepress/theme/mocha/sky.css";
 import "./global.scss";
 import Layout from "./Layout.vue";
 
