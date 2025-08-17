dd8316650f22c8ff2793121de3f6df6e2a5456dc
VChet <17050347+VChet@users.noreply.github.com>
2025-08-17 23:56:33 +0500

    chore: migrate to vitepress 2.0

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,9 @@
 // https://vitepress.dev/guide/custom-theme
 import DefaultTheme from "vitepress/theme";
-import type { Theme } from "vitepress";
 import "./global.scss";
 import Layout from "./Layout.vue";
 
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
   Layout
-} satisfies Theme;
+};
