1777fb448503d556e811be99122ce95efd19353e
VChet <17050347+VChet@users.noreply.github.com>
2025-08-29 00:17:14 +0500

    test(frontmatter): add 'en' articles

--- tests/frontmatter.test.ts
+++ tests/frontmatter.test.ts
@@ -12,31 +12,36 @@ describe("frontmatter validator", () => {
       .filter((fileName) => fileName.endsWith(".md"))
       .map((fileName) => join(dir, fileName));
   }
-  const ARTICLES_DIR = fileURLToPath(new URL("../articles", import.meta.url));
+  const ARTICLES_DIRS = [
+    fileURLToPath(new URL("../articles", import.meta.url)),
+    fileURLToPath(new URL("../en/articles", import.meta.url))
+  ];
 
   function isValidAuthor(value: unknown): boolean {
     const isAuthorExists = (payload: unknown): boolean => typeof payload === "string" && authorsData.has(payload);
     return Array.isArray(value) ? value.every(isAuthorExists) : isAuthorExists(value);
   }
 
-  for (const filePath of getMarkdownFiles(ARTICLES_DIR)) {
-    const raw = readFileSync(filePath, "utf8");
-    const result = matter(raw);
-    const frontmatter = result.data as Frontmatter;
+  for (const dir of ARTICLES_DIRS) {
+    for (const filePath of getMarkdownFiles(dir)) {
+      const raw = readFileSync(filePath, "utf8");
+      const result = matter(raw);
+      const frontmatter = result.data as Frontmatter;
 
-    it(basename(filePath), () => {
-      expect(frontmatter.title).toBeTypeOf("string");
-      expect(frontmatter.description).toBeTypeOf("string");
-      expect(isValidAuthor(frontmatter.author)).toBeTruthy();
-      expect(frontmatter.tags).toSatisfy(Array.isArray).and.not.toHaveLength(0);
+      it(basename(filePath), () => {
+        expect(frontmatter.title).toBeTypeOf("string");
+        expect(frontmatter.description).toBeTypeOf("string");
+        expect(isValidAuthor(frontmatter.author)).toBeTruthy();
+        expect(frontmatter.tags).toSatisfy(Array.isArray).and.not.toHaveLength(0);
 
-      if ("hero" in frontmatter) { expect(frontmatter.hero).toMatch(/^\/hero\/.+\.(png|jpe?g|webp|svg|gif)$/); }
-      if ("publish" in frontmatter) {
-        expect(frontmatter.publish).toBe(false);
-        expect(frontmatter.date).toBeUndefined();
-      } else {
-        expect(frontmatter.date).toBeInstanceOf(Date);
-      }
-    });
+        if ("hero" in frontmatter) { expect(frontmatter.hero).toMatch(/^\/hero\/.+\.(png|jpe?g|webp|svg|gif)$/); }
+        if ("publish" in frontmatter) {
+          expect(frontmatter.publish).toBe(false);
+          expect(frontmatter.date).toBeUndefined();
+        } else {
+          expect(frontmatter.date).toBeInstanceOf(Date);
+        }
+      });
+    }
   }
 });
