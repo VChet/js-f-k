2d2fe840ddbf6071f636f31c6df7bb1bbcdb4802
VChet <17050347+VChet@users.noreply.github.com>
2025-10-04 20:22:30 +0500

    feat(theme): add departure color theme

--- stylelint.config.js
+++ stylelint.config.js
@@ -20,9 +20,11 @@ export default {
   rules: {
     "@stylistic/color-hex-case": "lower",
     "at-rule-empty-line-before": "never",
+    "comment-empty-line-before": "never",
     "declaration-empty-line-before": "never",
     "rem-over-px/rem-over-px": [true, { ignore: ["0.5px", "1px", "font-size 16px", "font-size 20px"] }],
     "rule-empty-line-before": "never",
+    "scss/double-slash-comment-empty-line-before": "never",
     "selector-class-pattern": "[a-z]([a-z-]+)?(__([a-z]+-?)+)?(--([a-z]+-?)+){0,2}"
   }
 };
