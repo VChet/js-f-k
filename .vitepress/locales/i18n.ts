ba5f696cb6ea53ae7e8bef5733960b65d583580d
VChet <17050347+VChet@users.noreply.github.com>
2026-04-04 01:28:18 +0500

    feat(NotFound): handle missing translation

--- .vitepress/locales/i18n.ts
+++ .vitepress/locales/i18n.ts
@@ -4,13 +4,19 @@ export default {
     avatarAlt: "Аватар {}",
     tagTitle: "Перейти к тегу {}",
     lastUpdated: "Обновлено {}",
-    relatedArticles: "Похожие статьи"
+    relatedArticles: "Похожие статьи",
+    missingTranslation: "Отсутствует перевод",
+    missingTranslationHint: "Эта статья еще не переведена. Перейдите по ссылке, если вы хотите помочь с переводом:",
+    goToArticle: "Перейти к оригинальной статье"
   },
   en: {
     description: "HTML/TS/Vue — with examples, to the point, no fluff",
     avatarAlt: "{}'s avatar",
     tagTitle: "Go to tag {}",
     lastUpdated: "Last updated {}",
-    relatedArticles: "Related articles"
+    relatedArticles: "Related articles",
+    missingTranslation: "Missing translation",
+    missingTranslationHint: "This article is not translated yet. Follow the link if you want to help with translation:",
+    goToArticle: "Go to original article"
   }
 };
