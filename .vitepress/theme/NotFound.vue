d5c74d6773e023a53a809b46f53456ad65fa5206
VChet <17050347+VChet@users.noreply.github.com>
2026-04-04 01:20:19 +0500

    feat(layout): add default NotFound layout

---
+++ .vitepress/theme/NotFound.vue
@@ -0,0 +1,36 @@
+<!-- https://github.com/vuejs/vitepress/blob/main/src/client/theme-default/NotFound.vue -->
+<template>
+  <div class="NotFound">
+    <p class="code">
+      {{ theme.notFound?.code ?? '404' }}
+    </p>
+    <h1 class="title">
+      {{ theme.notFound?.title ?? 'PAGE NOT FOUND' }}
+    </h1>
+    <div class="divider" />
+    <blockquote class="quote">
+      {{
+        theme.notFound?.quote ??
+          "But if you don't change your direction, and if you keep looking, you may end up where you are heading."
+      }}
+    </blockquote>
+
+    <div class="action">
+      <a
+        class="link"
+        :href="withBase(theme.notFound?.link ?? '/')"
+        :aria-label="theme.notFound?.linkLabel ?? 'go to home'"
+      >
+        {{ theme.notFound?.linkText ?? 'Take me home' }}
+      </a>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { useData, withBase } from "vitepress";
+
+const { theme } = useData();
+</script>
+<style scoped>
+@import url("./not-found-default.css");
+</style>
